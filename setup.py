aa05d98106efbeb08a2c3438fc81492f19ed78b8
Bernardas <bernardas.alisauskas@gmail.com>
2016-06-30 10:52:35 +0000

    increment version to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="xonsh-vox-tabcomplete",
-    version="0.11",
+    version="0.2",
     license="MIT",
     url="https://github.com/Granitas/xonsh-vox-tabcomplete",
     description="vox tabcomplete support for the Xonsh shell",
