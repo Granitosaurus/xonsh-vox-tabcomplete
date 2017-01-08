04235a8531aa4facee7b684df68563345cddfb85
Bernardas <bernardas.alisauskas@gmail.com>
2017-01-08 22:49:44 +0000

    up version to 0.4

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 
 setup(
     name="xonsh-vox-tabcomplete",
-    version="0.3",
+    version="0.4",
     license="MIT",
-    url="https://github.com/Granitas/xonsh-vox-tabcomplete",
+    url="https://github.com/Granitosaurus/xonsh-vox-tabcomplete",
     description="vox tabcomplete support for the Xonsh shell",
     author="Bernardas Ališauskas",
     author_email="bernardas.alisauskas@gmail.com",
